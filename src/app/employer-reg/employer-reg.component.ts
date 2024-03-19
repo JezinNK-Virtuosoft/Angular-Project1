907b30baa1fce02612541fbbc044a9719fec12f7
JezinNK-Virtuosoft <162125878+JezinNK-Virtuosoft@users.noreply.github.com>
2024-03-19 10:52:27 +0530

    aded cchangess

--- src/app/employer-reg/employer-reg.component.ts
+++ src/app/employer-reg/employer-reg.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EmployerDetailsService } from '../shared/employer-details.service';
+import { EmployerDetails } from '../shared/employer-details';
 
 @Component({
   selector: 'app-employer-reg',
@@ -11,4 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class EmployerRegComponent {
 
+  EmployerDetail:EmployerDetails;
+  constructor(private employerServices:EmployerDetailsService){
+    this.EmployerDetail=new EmployerDetails();
+  }
 }
--- src/app/shared/employer-details.ts
+++ src/app/shared/employer-details.ts
@@ -3,10 +3,11 @@ export class EmployerDetails {
     eFirstName:string="";
     eMiddleName:string="";
     eLastName:string="";
+    email:string="";
     designation:string="";
-    phoneNumber:number=0;
+    phoneNumber:number |null=null;
     loginId:number=0;
-    companyID:number=0;
+    companyID:number | null=null;
     createdBy:string="";
     createdDate:Date=new Date();
     modifiedBy:string="";
